63c8d29303494b7fed81195939f6dda6d6db724e
Matthew Lin <mlin367@yahoo.com>
2018-10-27 17:32:49 -0700

    Worked on CSS for rating breakdown

--- client/src/components/StarListEntry.jsx
+++ client/src/components/StarListEntry.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const StarListEntry = props => (
   <li className="starListEntry">
-    <div className="starRating">5 STARS</div>
+    <div className="starRating"><a href="">5 STARS</a></div>
     <div className="starPercentage">
-      <div className="filled" />
+      <div className="filled"></div>
     </div>
     <div className="starCount">35</div>
   </li>
