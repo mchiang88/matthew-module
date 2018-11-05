1fe4565b8fe540aad279adcf1b74cf8e71ef9ade
Matthew Lin <mlin367@yahoo.com>
2018-11-05 15:09:51 -0800

    Added some instructions on readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 "# matthew-module" 
+
+-Run npm run seed and then 10-50 reviews will be created for each prodId
+-On refresh, componentDidMount will randomize the prodId state
+-The GET requests will then get data/comments for that particular prodId and render
--- database/seed/seedData.js
+++ database/seed/seedData.js
@@ -3,10 +3,6 @@ const Comments = require('../models');
 
 const fakeData = [];
 
-// Run npm run seed and then 10-50 reviews will be created for each prodId
-// On refresh, componentDidMount will randomize the prodId and the
-// GET requests will get data/comments for that particular prodId
-
 for (let j = 1; j <= 100; j++) {
   const randomNumber = Math.floor(Math.random() * 40 + 10);
   for (let i = 0; i < randomNumber; i++) {
